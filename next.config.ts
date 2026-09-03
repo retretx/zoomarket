a38e137b8d2b399031a7ee025241d62fdecd01ba
Nikita <retretx@gmail.com>
2026-09-03 17:42:15 +0500

    base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/zoomarket',
+  assetPrefix: '/zoomarket',
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
